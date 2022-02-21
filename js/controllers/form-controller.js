1cf60090532bae181358d0bb862d5adc24e7b5fe
Achette <i.achette@live.com>
2022-02-21 11:03:47 -0300

    Created folder models

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -1,5 +1,73 @@
-export function init() {
+import Address from "../models/address.js";
 
-    const inputCep = document.forms.newAddress.cep
-    console.group(inputCep)
+function State() {
+  this.Address = new Address();
+
+  this.btnSave = null;
+  this.btnClear = null;
+
+  this.inputCep = null;
+  this.inputStreet = null;
+  this.inputNumber = null;
+  this.inputCity = null;
+
+  this.errorCep = null;
+  this.errorNumber = null;
+}
+
+const state = new State();
+
+export function init() {
+  state.inputCep = document.forms.newAddress.cep;
+  state.inputStreet = document.forms.newAddress.street;
+  state.inputNumber = document.forms.newAddress.number;
+  state.inputCity = document.forms.newAddress.city;
+
+  state.btnSave = document.forms.newAddress.btnSave;
+  state.btnClear = document.forms.newAddress.btnClear;
+
+  state.errorCep = document.querySelector('[data-error="cep"');
+  state.errorNumber = document.querySelector('[data-error="number"');
+
+  state.inputNumber.addEventListener("change", handleInputNumberChange);
+  state.btnClear.addEventListener("clicke", handleBtnClearClick);
+  state.inputCep.addEventListener("change", handleInputCepChange);
+}
+
+function handleInputNumberChange(event) {
+  if (event.target.value == "") {
+    setFormError("number", "Campo requerido");
+  } else {
+    setFormError("number", "");
+  }
+}
+
+function handleBtnClearClick(event) {
+  event.preventDefault();
+  clearForm();
+}
+
+function clearForm() {
+  state.inputCep.value = "";
+  state.inputCity.value = "";
+  state.inputNumber.value = "";
+  state.inputCity.value = "";
+
+  setFormError("cep", "");
+  setFormError("number", "");
+
+  state.inputCep.focus();
+}
+
+function handleInputCepChange(event) {
+  if (event.target.value == "") {
+    setFormError("cep", "Campo requerido");
+  } else {
+    setFormError("cep", "");
+  }
+}
+
+function setFormError(key, value) {
+  const element = document.querySelector(`[data-error="${key}"`);
+  element.innerHTML = value;
 }
