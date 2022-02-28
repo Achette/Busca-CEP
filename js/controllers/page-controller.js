eadff4647ef080cfc960fd369538c247efcc8e8c
Achette <i.achette@live.com>
2022-02-28 13:01:31 -0300

    Fixed a bug in page controller file

--- js/controllers/page-controller.js
+++ js/controllers/page-controller.js
@@ -4,8 +4,6 @@ export function init() {
   const contactLink = document.querySelector(".contact-link");
 
   contactLink.addEventListener("click", handleContactLinkClick);
-
-  console.log("Page controller iniciado");
 }
 
 function handleContactLinkClick(event) {
